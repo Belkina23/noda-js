1e56099b150e6cd94f82a94a91db0209b3a9db95
Belkina23 <23zakat@gmail.com>
2023-04-01 13:25:02 +0300

    Update addContactShema.js

--- schemas/addContactShema.js
+++ schemas/addContactShema.js
@@ -7,9 +7,9 @@ const addContactShema = Joi.object({
 });
 
 const updateContactShema = Joi.object({
-  name: Joi.string().required(),
-  phone: Joi.string().required(),
-  email: Joi.string().required(),
+  name: Joi.string(),
+  phone: Joi.string(),
+  email: Joi.string(),
 }).or("name", "phone", "email");
 
 module.exports = { addContactShema, updateContactShema };
