6ebf58d7acaa1a83f75f623793500817f4010505
Belkina23 <23zakat@gmail.com>
2023-04-03 15:02:38 +0300

    правки

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,18 +1,29 @@
 const validation = (schema) => {
   return (req, res, next) => {
     const { error } = schema.validate(req.body);
-   
+    const { key } = error.details[0].context;
+
     if (error) {
       const errorTypes = error.details[0].type;
-      if (errorTypes === 'object.missing') {
-        error.status = 400;
-        error.message = 'missing fields';
-        next(error);
+
+      switch (errorTypes) {
+        case "object.missing":
+          error.status = 400;
+          error.message = `missing fields`;
+          next(error);
+          break;
+
+        case "any.required":
+          error.status = 400;
+          error.message = `missing required ${key} field`;
+          next(error);
+          break;
+
+        default:
+          break;
       }
-      
-    } else {
-      next();
     }
+    next();
   };
 };
 
