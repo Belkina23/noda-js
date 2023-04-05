f13e88ff40d09d61c0827698f0d04cc8c96775b9
Belkina23 <23zakat@gmail.com>
2023-04-05 13:43:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
-const {DB_HOST} = require('./config')
-const app = require('./app')
+const mongoose = require("mongoose");
+const { DB_HOST } = process.env;
+const app = require("./app");
 
-
-mongoose.connect(DB_HOST)
-.then(() => app.listen(3000))
-.catch(error => console.log(error.message));
+mongoose
+  .connect(DB_HOST)
+  .then(() => app.listen(3000))
+  .catch((error) => console.log(error.message));
