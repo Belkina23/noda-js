f6b321e0a91dc9e9502d74838e4a1d4672bd02b6
Belkina23 <23zakat@gmail.com>
2023-04-10 17:54:04 +0300

    добавила код к схеме

--- models/contacts.js
+++ models/contacts.js
@@ -24,6 +24,17 @@ const contactSchema = new Schema(
 
 contactSchema.post("save", handleMongooseError);
 
+contactSchema.pre('save', async function (next) {
+  const contact = this;
+  const existingContact = await Contact.findOne({ name: contact.name });
+  if (existingContact) {
+      const err = new Error('Name must be unique');
+      err.status = 400;
+      return next(err);
+  }
+  next();
+});
+
 const add = Joi.object({
   name: Joi.string().required(),
   phone: Joi.string().required(),
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,16 +2,16 @@ const express = require("express");
 const router = express.Router();
 const { contacts: ctrl } = require("../../controllers");
 const { validation, ctrlWrapper } = require("../../middlewares");
-const validateById = require('../../helpers/validateById')
+const validateById = require("../../helpers/validateById");
 const { schemaContacts } = require("../../models/contacts");
 
 router.get("/", ctrlWrapper(ctrl.getAll));
 
-router.get("/:id",validateById, ctrlWrapper(ctrl.getById));
+router.get("/:id", validateById, ctrlWrapper(ctrl.getById));
 
 router.post("/", validation(schemaContacts.add), ctrl.add);
 
-router.delete("/:id",validateById, ctrlWrapper(ctrl.deleteById));
+router.delete("/:id", validateById, ctrlWrapper(ctrl.deleteById));
 
 router.put(
   "/:id",
