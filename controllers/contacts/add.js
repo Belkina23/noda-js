32f519bb82e4781c78457d16ec4beb30e94c4242
Belkina23 <23zakat@gmail.com>
2023-04-12 13:59:47 +0300

    Update add.js

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -1,14 +1,21 @@
 const {Contact} = require("../../models/contacts");
 
 const add = async (req, res, next) => {
-    const exsistingContact = await Contact.findOne({ name: req.body.name });
-    if (exsistingContact) {
-      const err = new Error("Name must be unique");
-      err.status = 400;
-      return next(err);
-    }
-  const newContactAdd = await Contact.create(req.body);
-  res.status(201).json(newContactAdd);
-};
+  const { name, phone, email } = req.body;
+  const existingContact = await Contact.findOne({
+  $or: [{ name }, { phone }, { email }],
+  });
+  
+  if (existingContact) {
+  const err = new Error("Name, phone, and email must be unique");
+  err.status = 400;
+  return next(err);
+  }
+  
+  const newContact = await Contact.create(req.body);
+  res.status(201).json(newContact);
+  };
 
 module.exports = add;
+
+
